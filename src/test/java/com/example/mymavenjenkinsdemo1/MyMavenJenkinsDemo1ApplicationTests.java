1b95bfb7b3267bbd4a1073e3c7c079aac35573a0
panjianhao <2434732764@qq.com>
2022-11-10 14:21:11 +0800

    init...

---
+++ src/test/java/com/example/mymavenjenkinsdemo1/MyMavenJenkinsDemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mymavenjenkinsdemo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMavenJenkinsDemo1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
